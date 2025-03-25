d6bcb96d9ad777f3eb707ec4acb7ab720d09003c
Andrey22 <andreymail22112006@gmail.com>
2025-03-25 17:02:09 +0300

    feat: add carousel query

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -1,34 +1,53 @@
 function block(title) {
     const carousel = document.querySelector(`.${title} > .carousel`);
-    const cards = carousel.querySelectorAll('.products-items');
-    const dots = carousel.querySelectorAll('.dot');
+    const carouselItems = carousel.querySelector('.carousel-items');
+    const cards = carousel.querySelectorAll('a');
+    const dotsContainer = carousel.querySelector('.dots');
 
     const prev = carousel.querySelector('.button-previous');
     const next = carousel.querySelector('.button-next');
 
     let x = 0;
 
-    let step = 600 + parseFloat(getComputedStyle(document.documentElement).fontSize) * 24;
+    const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    const emGap = parseFloat(getComputedStyle(carouselItems).gap) / baseFontSize;
+    const cardWidth = parseFloat(getComputedStyle(cards[0].firstElementChild).width);
+    const carouselItemsWidth = parseFloat(getComputedStyle(carouselItems).width);
+    const cardsCount = Math.floor(carouselItemsWidth / cardWidth);
+    const cardsWidth = cardWidth * cardsCount;
+    const countOfBlocks = Math.ceil(cards.length / cardsCount);
+
+    const step = cardsWidth + emGap * baseFontSize * cardsCount;
+    const maxTranslate = -step * (countOfBlocks - 1);
+
+    dotsContainer.innerHTML = '';
+    for (let i = 0; i < countOfBlocks; i++) {
+        let dot = document.createElement('div');
+        dot.classList.add('dot');
+        dotsContainer.insertAdjacentHTML('beforeend', '<div class="dot"></div>');
+    }
 
     prev.addEventListener('click', function () {
         if (x + step <= 0) {
             x = x + step;
         }
-        cards.forEach((card) => (card.style.transform = `translateX(${x}px)`));
+        cards.forEach(card => card.style.transform = `translateX(${x}px)`);
         updateDots(x / -step);
     });
 
     next.addEventListener('click', function () {
-        if (x - step >= -(cards.length - 1) * step) {
+        console.log( x - step, maxTranslate );
+        if (x - step >= maxTranslate) {
             x = x - step;
         }
-        cards.forEach((card) => (card.style.transform = `translateX(${x}px)`));
+        cards.forEach(card => card.style.transform = `translateX(${x}px)`);
         updateDots(x / -step);
     });
 
     function updateDots(index) {
+        let dots = dotsContainer.querySelectorAll('.dot');
         dots.forEach((dot) => dot.removeAttribute('active'));
-        console.log(dots[index]);
+        console.log( index );
         dots[index].setAttribute('active', true);
     }
 
@@ -37,3 +56,8 @@ function block(title) {
 
 block('new-products');
 block('popular-products');
+
+window.addEventListener('resize', () => {
+    block('new-products');
+    block('popular-products');
+});
