06c3c18f012f9030818dcb0ee0c47174030b5659
Andrey22 <andreymail22112006@gmail.com>
2025-03-30 22:22:28 +0300

    feat: add increase products in bin

--- static/scripts/bin.js
+++ static/scripts/bin.js
@@ -1,12 +1,23 @@
+import { removeBinProduct, addBinProduct } from './updateLocalStorage.js';
+
+const form = document.querySelector('.form');
+const binProducts = document.querySelector('.bin-products');
 const table = document.querySelector('.products').tBodies[0];
 const itogSum = document.querySelector('.sum');
 
 function setBinProducts() {
-    const products = JSON.parse(localStorage.getItem('bin')) || [];
+    const products = JSON.parse(localStorage.getItem('bin'));
+    if (!products.length) {
+        binProducts.setAttribute('no-products', true);
+        form.style.display = 'none';
+        return;
+    }
+
+    binProducts.removeAttribute('no-products');
     products.forEach(product => {
         const productRow = `
-            <tr class="product-description">
-                <td class="delete-button">x</td>
+            <tr class="product-description" id="${product.id}">
+                <td><button class="delete-button">x</button></td>
                 <td class="photo"><img src="${product.img}" alt=""></td>
                 <td class="product-title">${product.title}</td>
                 <td class="price">${product.price} руб.</td>
@@ -19,7 +30,6 @@ function setBinProducts() {
                 </td>
                 <td class="itog">${product.price * product.count} руб.</td>
             </tr>`;
-        console.log( product );
         table.insertAdjacentHTML('beforeend', productRow);
     });
 
@@ -28,7 +38,52 @@ function setBinProducts() {
     function findSum() {
         return products.reduce((sum, product) => sum + product.price * product.count, 0);
     }
+};
+
+table.addEventListener('click', function(event) {
+    const deleteButton = event.target.closest('.delete-button');
+    const increaseButton = event.target.closest('.increase');
+    const deceaseButton = event.target.closest('.decrease');
+
+    const productDescription = event.target.closest('.product-description');
+
+    if (deleteButton) {
+        deleteProduct(productDescription);
+    } else if (increaseButton) {
+        increaseProduct(productDescription);
+    } else if (deceaseButton) {
+        decreaseProduct(productDescription);
+    }
+
+});
+
+function deleteProduct(productDescription) {
+    const productObj = createProductObject(productDescription);
+    productDescription.remove();
+    removeBinProduct(productObj);
+    setBinProducts();
 }
 
+function increaseProduct(productDescription) {
+    const productObj = createProductObject(productDescription);
+    const count = productDescription.querySelector('.buttons-container > span');
+    const sum = productDescription.querySelector('.itog');
+    count.textContent = productObj.count + 1; 
+    sum.textContent = `${(productObj.count + 1) * productObj.price} руб.`;
+    console.log( productObj );
+    addBinProduct(productObj);
+}
+
+function decreaseProduct(productDescription) {
+
+}
+
+function createProductObject(elem) {
+    return { 
+        id: +elem.id,
+        price: parseFloat(elem.querySelector('.price').textContent),
+        count: +elem.querySelector('.buttons-container > span').textContent
+    };
+}
 
 setBinProducts();
