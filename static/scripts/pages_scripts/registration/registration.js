2e8e71a584c70c660e44de732697d42eefc357a7
Andrey22 <andreymail22112006@gmail.com>
2025-04-20 18:46:10 +0300

    fix: update registration

--- static/scripts/pages_scripts/registration/registration.js
+++ static/scripts/pages_scripts/registration/registration.js
@@ -5,11 +5,11 @@ const password = document.getElementById('password');
 const registrationButton = document.querySelector('[type="submit"]');
 
 registrationButton.addEventListener('click', async (event) => {
+    event.preventDefault();
     let emailText = email.value;
     let passwordText = password.value;
 
     let userId = await registration(emailText, passwordText);
     console.log( userId );
 
-    event.preventDefault();
 });
